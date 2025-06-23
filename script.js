fc6f5ab6985a4a2deb08173476a0b5711ab0625a
Mangesh Bodke <mangeshbodke2025@gmail.com>
2025-06-23 19:46:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,18 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
   
+  const prices = document.querySelectorAll(".price");
+
+  let sum = 0;
+
+
+  for (let i = 0; i < prices.length - 1; i++) {
+    sum += Number(prices[i].textContent);
+  }
+
+
+  prices[prices.length - 1].textContent = sum;
 };
 
 getSumBtn.addEventListener("click", getSum);
-
